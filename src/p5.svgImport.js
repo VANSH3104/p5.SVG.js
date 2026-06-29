7e1d321f3a45126c3a4936ad69cd40ecbe6c3d86
VANSH3104 <vanshkabra05@gmail.com>
2026-06-29 15:58:28 +0530

    Adding Svg parse to convert svg into recorder

--- src/p5.svgImport.js
+++ src/p5.svgImport.js
@@ -1,4 +1,4 @@
-import { ShapeRecorder } from "./p5.ShapeRecorder.js";
+import { ShapeRecorder, ShapeNode } from "./p5.ShapeRecorder.js";
 
 export function SVGImportAddon(p5, fn, lifecycles) {
     class SVGImporter {
@@ -9,7 +9,7 @@ export function SVGImportAddon(p5, fn, lifecycles) {
         import(svgText) {
             const parser = new DOMParser();
             const doc = parser.parseFromString(svgText, "image/svg+xml");
-
+            this.visit(doc.documentElement);
             console.log(doc.documentElement);
 
             // Next step:
@@ -17,6 +17,41 @@ export function SVGImportAddon(p5, fn, lifecycles) {
 
             return this.recorder.getRecord();
         }
+        visit(node) {
+            this.parseStyle(node);
+            switch (node.tagName) {
+                case "svg":
+                    return this.visitSVG(node);
+                case "g":
+                    return this.visitGroup(node);
+            }
+        }
+        visitSVG(node) {
+            for (const child of node.children) {
+                this.visit(child);
+            }
+        }
+
+        visitGroup(node) {
+            this.recorder.enterScope();
+
+            for (const child of node.children) {
+                this.visit(child);
+            }
+
+            this.recorder.leaveScope();
+        }
+
+        createShape(builder) {
+            const shape = new p5.Shape({
+                position: new p5.Vector(0, 0)
+            });
+            shape.beginShape();
+            builder(shape);
+            shape.endShape();
+            return shape;
+        }
+
     }
 
     // SVG IMPORT api
